bb3eacc86d32ced3e91d78e6144255b873ebe6e6
Md-Kutub-Islam <mdkutubislam09@gmail.com>
2024-11-13 22:58:04 +0530

    update multer

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -2,20 +2,36 @@
   after using this multer middleware this will add image in req object so in the corresponding controller the image can be retrieved using the 'req.file' or 'req.files' according to number of images. if any problem then just log the 'req.file' in the console and you'll get the idea.
 */
 
-import path from "path";
+// import path from "path";
+// import multer from "multer";
+// import { allowedImgExtensionsEnum } from "../constants.js";
+
+// export const upload = multer({
+//   dest: "temp/",
+//   storage: multer.diskStorage({
+//     destination: "tmp/",
+//     filename: (_, file, done) => {
+//       done(null, file.originalname);
+//     },
+//   }),
+//   fileFilter: (_, file, done) => {
+//     let ext = path.extname(file.originalname);
+//     if (!allowedImgExtensionsEnum.includes(ext)) {
+//       done(new Error(`Unsupported image type! ${ext}`), false);
+//       return;
+//     }
+//     done(null, true);
+//   },
+// });
+
 import multer from "multer";
+import path from "path";
 import { allowedImgExtensionsEnum } from "../constants.js";
 
 export const upload = multer({
-  dest: "temp/",
-  storage: multer.diskStorage({
-    destination: "tmp/",
-    filename: (_, file, done) => {
-      done(null, file.originalname);
-    },
-  }),
+  storage: multer.memoryStorage(), // Use memory storage to avoid writing to disk
   fileFilter: (_, file, done) => {
-    let ext = path.extname(file.originalname);
+    const ext = path.extname(file.originalname);
     if (!allowedImgExtensionsEnum.includes(ext)) {
       done(new Error(`Unsupported image type! ${ext}`), false);
       return;
